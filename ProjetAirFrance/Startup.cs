d3c48297aba1151a35128de9b905908f3013b26c
KevinC <kevin.chapelet2017@campus-eni.fr>
2019-02-14 13:30:47 +0100

    1st commit

---
+++ ProjetAirFrance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetAirFrance.Startup))]
+namespace ProjetAirFrance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
