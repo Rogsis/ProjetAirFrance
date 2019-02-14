3d9e3721343703bc93b4dd0fd442ed9878c554bf
MADEC <delasalle.sio.madec.f@gmail.com>
2019-02-14 14:50:29 +0100

    debug florian

--- ProjetAirFrance/Controllers/HomeController.cs
+++ ProjetAirFrance/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace ProjetAirFrance.Controllers
 
             return View();
         }
+
+        public ActionResult Trajets()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
